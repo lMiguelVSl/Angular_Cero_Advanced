f4f596a234458e53fcd2808044ba86c7d3a7c4e9
Miguel Vargas <miguel.vargas.work@outlook.com>
2024-02-13 19:23:10 -0500

    Auth Service new method and get

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environments } from "src/environments/environments";
 import { User } from "../interfaces/user.interface";
+import { Observable, tap } from "rxjs";
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
@@ -10,4 +11,18 @@ export class AuthService {
     private user?: User;
 
     constructor(private http: HttpClient) { }
+
+    get currentUser(): User | undefined {
+        if (!this.user) return undefined;
+        return structuredClone(this.user);
+    }
+
+    login(): Observable<User> {
+        return this.http.get<User>(`${this.baseUrl}/users/1`)
+            .pipe(
+                tap(user => this.user = user),
+                tap(user => localStorage.setItem('token', user.id.toString()))
+            );
+    }
+
 }
