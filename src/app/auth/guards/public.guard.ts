c52e7069f9e772c629d2d837306f630003aca06d
Miguel Vargas <miguel.vargas.work@outlook.com>
2024-02-20 21:27:11 -0500

    public guard for user authenticated

---
+++ src/app/auth/guards/public.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, CanMatch, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from "@angular/router";
+import { Observable, tap } from "rxjs";
+import { AuthService } from "../services/auth.service";
+
+@Injectable({ providedIn: 'root' })
+export class PublicGuard implements CanActivate {
+
+    constructor(private authService: AuthService, private router: Router) { }
+
+    private checkAuthStatus(): boolean | Observable<boolean> {
+        return this.authService.checkAuthentication()
+            .pipe(
+                tap(isAuthenticated => {
+                    if (isAuthenticated) this.router.navigate(['/heroes'])
+                })
+            )
+    }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+        console.log('Can Activate public guard');
+        console.log({ route, state });
+
+        return this.checkAuthStatus();
+    }
+
+}
